4fc7b81c31778754900e5e06f750588655d4c90f
pyephyohan <pyephyohan@gmail.com>
2025-07-11 13:43:25 +0700

    Remove config_credentials.txt from repo

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,15 +2,14 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
-
 const firebaseConfig = {
-  apiKey: "AIzaSyDdtldI2fB64mZly2TjkuhL29NOtEZs42A",
-  authDomain: "react-login-ui-firebase.firebaseapp.com",
-  databaseURL: "https://react-login-ui-firebase-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "react-login-ui-firebase",
-  storageBucket: "react-login-ui-firebase.firebasestorage.app",
-  messagingSenderId: "724627283543",
-  appId: "1:724627283543:web:443d11307029d52f0b8d12"
+  apiKey: "AIzaSyXXXXXX-REPLACE-WITH-YOURS",
+  authDomain: "your-project-id.firebaseapp.com",
+  databaseURL: "https://your-project-id-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "your-project-id",
+  storageBucket: "your-project-id.appspot.com",
+  messagingSenderId: "123456789000",
+  appId: "1:123456789000:web:abcdef1234567890"
 };
 
 // Initialize Firebase
